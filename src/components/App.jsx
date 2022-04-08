24c60589a00aaa0aa6b9b0f41b5c019ba12218f1
Frank Villanueva <frank05111996@gmail.com>
2022-04-08 17:52:47 -0500

    html loader and css loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,7 @@
-import react from "react";
-
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World 2</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import react from "react";
-import reactDom from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./components/App";
+import './styles/global.scss';
 
-reactDom.render(<App />, document.getElementById("root"));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
 
